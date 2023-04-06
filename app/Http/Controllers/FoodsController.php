7e6565799ac2a7e97f40e2b104a7aa3c5e9770b0
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2023-04-06 21:28:58 +0700

    Up code

--- app/Http/Controllers/FoodsController.php
+++ app/Http/Controllers/FoodsController.php
@@ -8,10 +8,17 @@ use App\Models\Food;
 class FoodsController extends Controller
 {
     public function index (){
-        $foods = Food::all();
+        // $foods = Food::all(); //lay tat ca trong ban ghi
+        $foods = Food::where('name', '=', 'sushi') //lay san pham co ten la sushi
+                // ->firstOrFail();    
+                   -> get();
         // dd($foods);
         return view('foods.index', [
             'foods' => $foods
         ]);
+    }
+
+    public function create(){
+        return view('foods.create');
     }
 }
